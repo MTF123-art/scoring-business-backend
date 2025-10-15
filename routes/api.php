0e4b75e892efff6fbc98ddd0115a2d2789d6d260
muqtafi <ahmadmuqtafi58@gmail.com>
2025-10-15 07:40:36 +0700

    refactor: move profile-related methods to ProfileController and update routes

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,14 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\FacebookController;
 use App\Http\Controllers\Api\InstagramController;
+use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\ScoreController;
-use App\Models\Score;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return response()->json(['message' => 'API is running']);
 });
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -40,13 +35,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::prefix('profile')->group(function () {
-        Route::get('/', [AuthController::class, 'getProfile']);
-        Route::post('/', [AuthController::class, 'updateProfile']);
-        Route::post('/change-password', [AuthController::class, 'changePassword']);
+        Route::get('/', [ProfileController::class, 'getProfile']);
+        Route::post('/', [ProfileController::class, 'updateProfile']);
+        Route::post('/change-password', [ProfileController::class, 'changePassword']);
     });
 
 });
-Route::get('/user/avatar/{id}', [AuthController::class, 'getAvatarById']);
+Route::get('/user/avatar/{id}', [ProfileController::class, 'getAvatarById']);
 
 Route::get('/instagram/callback', [InstagramController::class, 'handleCallback']);
 Route::get('/facebook/callback', [FacebookController::class, 'handleCallback']);
